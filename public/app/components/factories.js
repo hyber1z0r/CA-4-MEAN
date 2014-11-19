1200c815e2d7d41e118c8aedaa46091aa3a45342
MessiKrkic <MessiKrkic@gmail.com>
2014-11-19 11:38:10 +0100

    Added the factory

--- public/app/components/factories.js
+++ public/app/components/factories.js
@@ -1,14 +1,62 @@
 'use strict';
 
-/* Factories */
 
-angular.module('myAppRename.factories', []).
-  factory('InfoFactory', function () {
-    var info = "Hello World from a Factory";
-    var getInfo = function getInfo(){
-      return info;
-    }
+/* Factories */
+var app = angular.module('wikiFactory', []);
+
+app.factory('WikiFactory', function ($http) {
+
+    var getWiki = function (title, callback) {
+        $http.get("http://localhost:3000/api/wiki?title=" + title)
+            .success(function (data) {
+                callback(null, data);
+            })
+            .error(function (err) {
+                return callback(err);
+            });
+
+    };
+
+    var findWiki = function (search, callback) {
+        $http.get("http://localhost:3000/api/findWiki?q=" + search)
+            .success(function (data) {
+                callback(null, data);
+            })
+            .error(function (err) {
+                return callback(err);
+            });
+
+    };
+
+    var allCategories = function (callback) {
+        $http.get("http://localhost:3000/api/categories")
+            .success(function (data) {
+                callback(null, data);
+            })
+            .error(function (err) {
+                return callback(err);
+            });
+
+    };
+
+    var searchCategories = function (search, callback) {
+            $http.get("http://localhost:3000/api/categories?q=" + search)
+                .success(function (data) {
+                    callback(null, data);
+                })
+                .error(function (err) {
+                    return callback(err);
+                });
+
+    };
+
+
+
+
     return {
-      getInfo: getInfo
+        getWiki: getWiki,
+        findWiki: findWiki,
+        allCategories: allCategories,
+        searchCategories: searchCategories
     }
   });
